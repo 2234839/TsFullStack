090e5f00f1892c5fc09f150a5ec388a3ae685b9b
崮生（子虚） <2234839456@qq.com>
2025-04-13 16:02:22 +0000

    init admin-panel

---
+++ apps/admin-panel/src/App.vue
@@ -0,0 +1,8 @@
+<style scoped></style>
+<template>
+  <Button>33</Button>
+</template>
+<script setup lang="ts">
+  import Button from 'primevue/button';
+  console.log('[Button]', Button);
+</script>
