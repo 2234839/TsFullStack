09effd797b60f6b2359088c00e4b41f7755b6be0
崮生（子虚） <2234839456@qq.com>
2025-04-13 16:03:39 +0000

    clean

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -1,14 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 import { enhance } from '@zenstackhq/runtime/edge';
-import ModelMeta from '@zenstackhq/runtime/model-meta';
+import __ModelMeta from '@zenstackhq/runtime/model-meta';
 
 // @ts-expect-error  修复默认导出问题
-const ModelMetaFix: typeof ModelMeta = ModelMeta['default'] ? ModelMeta['default'] : ModelMeta;
+const ModelMeta: typeof __ModelMeta = __ModelMeta['default']
+  ? // @ts-expect-error
+    __ModelMeta['default']
+  : __ModelMeta;
+
+/** 是否开启数据库调试日志   */
+const DB_DEBUG = false;
 
 /** 尽量不要使用这个对象，除非你确定不需要鉴权。否则应该使用 getPrisma 函数 */
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({ log: ['info'] });
 /** 获取所有模型名称   */
-const modelsName = Object.keys(ModelMetaFix.models) as (keyof typeof ModelMeta.models)[];
+const modelsName = Object.keys(ModelMeta.models) as (keyof typeof __ModelMeta.models)[];
 /** 只允许这些方法通过代理访问, 默认为 $transaction 和对应的表 */
 const allowedMethods = ['$transaction', ...modelsName] as const;
 
@@ -26,7 +32,7 @@ export async function getPrisma({ userId }: { userId: string }) {
     throw new Error('User not found');
   }
 
-  const db = enhance(prisma, { user }, { logPrismaQuery: true });
+  const db = enhance(prisma, { user }, { logPrismaQuery: DB_DEBUG });
 
   /** 代理对象，限制对 Prisma 客户端的访问方法  */
   const dbProxy = new Proxy(db, {
@@ -37,6 +43,7 @@ export async function getPrisma({ userId }: { userId: string }) {
       return Reflect.get(target, prop, receiver);
     },
   }) as Pick<typeof db, (typeof allowedMethods)[number]>;
+
   return {
     db: dbProxy,
     user: {
