6cd9bf9e2039c78c23d4b6bbbd752799078fb76d
崮生（子虚） <2234839456@qq.com>
2025-04-13 23:59:15 +0800

    迁移到正确的 apps 路径，完成客户端api联调调用，完善 rpc 类型支持

--- app/backend/src/lib/index.ts
+++
@@ -1,1 +0,0 @@
-export type { API } from '../api';
--- apps/backend/src/rpc/index.ts
+++ apps/backend/src/rpc/index.ts
@@ -5,8 +5,28 @@ import type { Effect } from 'effect';
 interface commonOptions {
   middleware?: ((method: string, data: any[], next: () => Promise<any>) => Promise<any>)[]; // 统一的中间件
 }
-
-export async function createRPC<API_TYPE>(
+/** 服务端远程调用，api 返回值不会解开 Effect 类型 */
+export function createRPC<API_TYPE>(
+  type: 'apiProvider',
+  options: commonOptions & {
+    genApiModule: () => Promise<API_TYPE>;
+  },
+): {
+  API: DeepAsyncify<API_TYPE>;
+  RC: (method: string, data: any[]) => Promise<DeepReturnTypeUnion<API_TYPE>>;
+};
+/** 客户端远程调用，api 返回值会解开 Effect 类型，这是由服务端的代码去实现的 Effect 处理 */
+export function createRPC<API_TYPE>(
+  type: 'apiConsumer',
+  options: commonOptions & {
+    /** 配置此选项替换默认的远程调用函数，默认逻辑采用 fetch 实现。 */
+    remoteCall: (method: string, data: any[]) => Promise<any>; // 远程调用函数
+  },
+): {
+  API: DeepAsyncEffect<API_TYPE>;
+  RC: (method: string, data: any[]) => Promise<DeepUnEffectReturnTypeUnion<API_TYPE>>;
+};
+export function createRPC<API_TYPE>(
   ...[type, options]:
     | [
         'apiProvider',
@@ -22,8 +42,6 @@ export async function createRPC<API_TYPE>(
         },
       ]
 ) {
-  const apiModule = type === 'apiProvider' ? await options.genApiModule() : undefined;
-
   const remoteCall = type === 'apiConsumer' ? options.remoteCall : undefined;
 
   async function RC<K extends string>(
@@ -42,6 +60,8 @@ export async function createRPC<API_TYPE>(
     async function executeCall(): Promise<any> {
       try {
         if (type === 'apiProvider') {
+          const apiModule = type === 'apiProvider' ? await options.genApiModule() : undefined;
+
           const methodParts = method.split('.');
           let currentObj: any = apiModule;
           for (const part of methodParts) {
@@ -98,12 +118,20 @@ export async function createRPC<API_TYPE>(
 export type AsyncifyReturnType<T> = T extends (...args: any[]) => infer R
   ? (...args: Parameters<T>) => Promise<Awaited<R>>
   : T;
-
-/** 因为如果是客户端调用，那么返回值肯定要是 promise （网络异步的特性决定的），所以使用这个类型来将所有返回值的类型包裹一层promise */
+export type AsyncifyUnEffectReturnType<T> = T extends (...args: any[]) => infer R
+  ? (...args: Parameters<T>) => Promise<ExtractEffectSuccess<Awaited<R>>>
+  : T;
+/** 因为如果是远程调用，那么返回值肯定要是 promise （网络异步的特性决定的），所以使用这个类型来将所有返回值的类型包裹一层promise */
 export type DeepAsyncify<T> = T extends (...args: any[]) => any
   ? AsyncifyReturnType<T>
   : T extends object
   ? { [K in keyof T]: DeepAsyncify<T[K]> }
+  : T;
+/** 如果是客户端调用，服务端必须要已经处理了 Effect ，所以返回的就是解开来 Effect 的*/
+export type DeepAsyncEffect<T> = T extends (...args: any[]) => any
+  ? AsyncifyUnEffectReturnType<T>
+  : T extends object
+  ? { [K in keyof T]: DeepAsyncEffect<T[K]> }
   : T;
 
 /** 获取一个深度嵌套的对象（apis）上的函数的返回值联合类型
@@ -115,5 +143,10 @@ export type DeepReturnTypeUnion<T> = T extends (...args: any[]) => any
   ? { [K in keyof T]: DeepReturnTypeUnion<T[K]> }[keyof T]
   : never;
 
+export type DeepUnEffectReturnTypeUnion<T> = T extends (...args: any[]) => any
+  ? ExtractEffectSuccess<Awaited<ReturnType<T>>>
+  : T extends object
+  ? { [K in keyof T]: DeepUnEffectReturnTypeUnion<T[K]> }[keyof T]
+  : never;
 /** 解开可能是 Effect 的返回值，如果是 Effect 则返回其成功类型，否则返回原类型 */
 export type ExtractEffectSuccess<T> = T extends Effect.Effect<infer A, infer E, infer P> ? A : T;
