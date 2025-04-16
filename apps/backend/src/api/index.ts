f216a3b3e9cb74907e39e2e353aab457b29e0ea8
崮生（子虚） <2234839456@qq.com>
2025-04-16 13:26:06 +0000

    clean

--- apps/backend/src/api/index.ts
+++ apps/backend/src/api/index.ts
@@ -9,7 +9,7 @@ export const apis = {
       return ModelMeta;
     },
   },
-  // 直接获取数据库 db 操作对象,这个函数仅用于给 Effect 提供类型提示，实际不应使用， server/index.ts 中会覆盖此变量交给用户，覆盖之后的类型参考下面的  API 类型
+  // 直接获取数据库 db 操作对象,这个函数仅用于给 Effect 提供 apis 依赖 AuthService 的类型提示 ， server/index.ts 中会覆盖此变量交给用户，覆盖之后的类型参考下面的  API 类型
   db() {
     return Effect.gen(function* () {
       const auth = yield* AuthService;
