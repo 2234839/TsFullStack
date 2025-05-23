7dba4d9ad83219c3ca01ed20bedbdff61db9ef36
崮生（子虚） <2234839456@qq.com>
2025-05-24 07:34:48 +0800

    添加自动组件引入

--- apps/admin-console/vite.config.ts
+++ apps/admin-console/vite.config.ts
@@ -4,9 +4,19 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+
+import { PrimeVueResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss(), vueDevTools()],
+  plugins: [
+    Components({ resolvers: [PrimeVueResolver()], dts: true }),
+    vue(),
+    vueJsx(),
+    tailwindcss(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
