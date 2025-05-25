5418220f7dc290541a3ca26be65bb5b2dc623d1a
崮生（子虚） <2234839456@qq.com>
2025-05-25 17:21:40 +0800

    重构 server 代码，全链路引入 effect，不过现在对于错误的处理可能不正确

---
+++ apps/backend/src/service/AppConfigService.ts
@@ -0,0 +1,24 @@
+import { Context } from 'effect';
+export class AppConfigService extends Context.Tag('AppConfigService')<
+  AppConfigService,
+  AppConfig
+>() {}
+
+/** 请求上下文数据 */
+export type AppConfig = {
+  /** 上传文件的目录 */
+  uploadDir: string;
+};
+
+// /** 确保所有配置项可用 */
+// export async function seedConfig() {
+//   const { config } = await loadConfig({});
+
+//   const dirExists = await fs.stat(AppConfig.uploadDir).catch(() => null);
+//   if (!dirExists) {
+//     await fs.mkdir(AppConfig.uploadDir, { recursive: true });
+//     console.log(`Upload directory ${AppConfig.uploadDir} is ready`);
+//   } else if (!dirExists.isDirectory()) {
+//     throw new Error(`Upload directory ${AppConfig.uploadDir} is not a directory`);
+//   }
+// }
