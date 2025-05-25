cd380bcf6c0d29f818836b0d7b0e81e98cdbf517
崮生（子虚） <2234839456@qq.com>
2025-05-25 19:17:09 +0800

    重构配置项

--- apps/backend/src/db/seed.ts
+++ apps/backend/src/db/seed.ts
@@ -1,90 +1,77 @@
 import { compareSync, hashSync } from 'bcryptjs';
-import { getPrisma, prisma } from '.';
+import { Effect } from 'effect';
+import { prisma } from '.';
+import { AppConfigService } from '../service/AppConfigService';
 
 /** 对数据库进行一些初始化设置 */
-export async function seedDB() {
-  await seedAdmin();
-}
-
-/** 超级管理员账户 */
-export const systemAdminUser = {
-  email: 'admin@example.com',
-  password: 'adminpassword123', // Ensure this is hashed in a real application
-};
-
-let awaitSeedAdminR: (p: any) => void;
-const awaitSeedAdminFlag = new Promise((r) => {
-  awaitSeedAdminR = r;
-});
-
-/** 具有系统管理员身份的数据库操作实例 */
-export const systemAdminDB = new Promise<ReturnType<typeof getPrisma>>(async (r) => {
-  await awaitSeedAdminFlag;
-  const db = getPrisma({ email: systemAdminUser.email });
-  r(db);
+export const seedDB = Effect.gen(function* () {
+  yield* seedAdmin;
 });
 
 /** 创建管理员账户及其角色 */
-async function seedAdmin() {
-  let admin = await prisma.user.findUnique({
-    where: {
-      email: systemAdminUser.email,
-    },
-    include: {
-      role: true,
-    },
-  });
-
-  if (!admin) {
-    admin = await prisma.user.create({
-      data: {
-        email: systemAdminUser.email,
-        password: systemAdminUser.password,
-        role: {
-          connectOrCreate: {
-            where: { name: 'admin' },
-            create: { name: 'admin' },
-          },
-        },
-      },
-      include: {
-        role: true,
-      },
-    });
-  } else if (!compareSync(systemAdminUser.password, admin.password)) {
-    console.log('重置系统管理员帐号密码');
-    admin = await prisma.user.update({
+const seedAdmin = Effect.gen(function* () {
+  const { systemAdminUser } = yield* AppConfigService;
+  let admin = yield* Effect.promise(() =>
+    prisma.user.findUnique({
       where: {
         email: systemAdminUser.email,
       },
-      data: {
-        password: hashSync(systemAdminUser.password),
-      },
       include: {
         role: true,
       },
-    });
+    }),
+  );
+
+  if (!admin) {
+    admin = yield* Effect.promise(() =>
+      prisma.user.create({
+        data: {
+          email: systemAdminUser.email,
+          password: systemAdminUser.password,
+          role: {
+            connectOrCreate: {
+              where: { name: 'admin' },
+              create: { name: 'admin' },
+            },
+          },
+        },
+        include: {
+          role: true,
+        },
+      }),
+    );
+  } else if (!compareSync(systemAdminUser.password, admin.password)) {
+    console.log('重置系统管理员帐号密码');
+    admin = yield* Effect.promise(() =>
+      prisma.user.update({
+        where: {
+          email: systemAdminUser.email,
+        },
+        data: {
+          password: hashSync(systemAdminUser.password),
+        },
+        include: {
+          role: true,
+        },
+      }),
+    );
   }
   if (!admin.role.find((el) => el.name === 'admin')) {
     console.log('添加管理员角色');
-    await prisma.user.update({
-      where: {
-        email: systemAdminUser.email,
-      },
-      data: {
-        role: {
-          connectOrCreate: {
-            where: { name: 'admin' },
-            create: { name: 'admin' },
+    yield* Effect.promise(() =>
+      prisma.user.update({
+        where: {
+          email: systemAdminUser.email,
+        },
+        data: {
+          role: {
+            connectOrCreate: {
+              where: { name: 'admin' },
+              create: { name: 'admin' },
+            },
           },
         },
-      },
-    });
+      }),
+    );
   }
-  awaitSeedAdminR(1);
-}
-
-// 测试代码 自动重建管理员帐号 TODO 应该要通过环境变量控制此代码执行
-setInterval(() => {
-  seedAdmin();
-}, 1000 * 10);
+});
