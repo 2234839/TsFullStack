84fa0ffc53265c068b71e2208d35dd2afc229ce1
崮生（子虚） <2234839456@qq.com>
2025-06-08 09:55:24 +0800

    实现 encryptSerializer 强化本地存储机密信息安全 实装记住账号功能

--- apps/website-frontend/src/storage/index.ts
+++ apps/website-frontend/src/storage/index.ts
@@ -1,20 +1,35 @@
 import { routeMap, routerUtil } from '@/router';
-import { StorageSerializers, useStorage } from '@vueuse/core';
+import { encryptSerializer } from '@/utils/encryptSerializer';
+import { StorageSerializers, useStorage, useStorageAsync } from '@vueuse/core';
 import { computed, watchEffect } from 'vue';
 
 const appId = 'tfs_';
 /** 用户认证信息存储  */
-export const authInfo = useStorage<
+export const authInfo = useStorageAsync<
   | {
       userId: string;
       token: string;
       expiresAt: number;
     }
   | undefined
->(appId + 'authInfo', null, undefined, { serializer: StorageSerializers.object });
+>(appId + 'authInfo_v0', null, undefined, { serializer: encryptSerializer });
 export const authInfo_isLogin = computed(() => {
   return !!authInfo.value && authInfo.value.expiresAt > Date.now();
 });
+
+export const localUserPwd = useStorageAsync<{
+  username: string;
+  password: string;
+  rememberMe: boolean;
+}>(
+  /** 故意使用 test 作为 key ，减少社工风险 */ appId + '__test__',
+  { username: '', password: '', rememberMe: false },
+  undefined,
+  {
+    serializer: encryptSerializer,
+  },
+);
+
 /** 清楚认证信息并跳转到登录页面  */
 export function authInfo_logout(/** 登录后重定向的页面地址   */ r?: string) {
   authInfo.value = undefined;
@@ -50,7 +65,5 @@ watchEffect(() => {
 export const i18nStore = useStorage<'zh-CN' | 'en'>(appId + 'i18nStore', null);
 //#endregion
 
-
 /** 用于控制悬浮github star按钮显示隐藏的storage key值 */
 export const githubStarShow = useStorage<'show' | 'hide'>(appId + 'githubStarShow', 'show');
-
