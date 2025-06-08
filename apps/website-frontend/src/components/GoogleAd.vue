e9908c9288873a5bfe1c024cd4bcf39ef98d3753
崮生（子虚） <2234839456@qq.com>
2025-06-08 12:31:30 +0800

    添加广告组件

---
+++ apps/website-frontend/src/components/GoogleAd.vue
@@ -0,0 +1,19 @@
+<style scoped></style>
+<template>
+  <!-- 展示广告位 -->
+  <ins
+    class="adsbygoogle"
+    style="display: block"
+    data-ad-client="ca-pub-7181140659254262"
+    data-ad-slot="8223616080"
+    data-ad-format="auto"
+    data-full-width-responsive="true"></ins>
+</template>
+<script setup lang="ts">
+  import { onMounted } from 'vue';
+
+  onMounted(() => {
+    // @ts-expect-error
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  });
+</script>
