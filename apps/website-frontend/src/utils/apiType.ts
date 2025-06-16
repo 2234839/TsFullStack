b0d280df244dacee1c2c987c3ea14f218cf94091
崮生（子虚） <2234839456@qq.com>
2025-06-16 11:16:53 +0000

    前端存储更多用户信息,对于测试账户添加顶部横幅警告

---
+++ apps/website-frontend/src/utils/apiType.ts
@@ -0,0 +1,6 @@
+import type { DeepAsyncEffect } from 'tsfullstack-backend/dist-lib/rpc';
+import { type API as __API__, type AppAPI as __AppAPI__ } from 'tsfullstack-backend';
+
+type AppAPI = DeepAsyncEffect<__AppAPI__>;
+type ApiResponseType<T extends (...args: any[]) => any> = Awaited<ReturnType<T>>;
+export type loginByEmailPwd_res = ApiResponseType<AppAPI['system']['loginByEmailPwd']>;
