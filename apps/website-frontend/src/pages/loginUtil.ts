d967dfa4561145b7747e9038a5644bd0a5d13226
崮生（子虚） <2234839456@qq.com>
2025-06-18 11:08:04 +0000

    为测试帐号添加登录新帐号引导

--- apps/website-frontend/src/pages/loginUtil.ts
+++ apps/website-frontend/src/pages/loginUtil.ts
@@ -4,18 +4,28 @@ import type { loginByEmailPwd_res } from '@/utils/apiType';
 
 /** 登录成功后的跳转逻辑 */
 export function loginGoto(
-  info: loginByEmailPwd_res,
+  info?: loginByEmailPwd_res,
   options?: {
     /** 重定向地址 */
     r?: string;
   },
 ) {
-  authInfo.value = info;
+  if (info) {
+    authInfo.value = info;
+  }
 
   if (options?.r) {
     const redirectUrl = decodeURIComponent(options.r);
     location.href = redirectUrl; // 直接跳转到指定的URL
     return; // 阻止后续的跳转逻辑
   }
-  routerUtil.push(routeMap.admin, {});
+  routerUtil.push(routeMap.index, {});
+}
+
+/** 跳转到登录页面 */
+export function gotoLogin(options?: {
+  /** 重定向地址 */
+  r?: string;
+}) {
+  routerUtil.push(routeMap.login, { r: options?.r });
 }
