c1a2aa13042d4b7d5a6be6efcceacb3f52e4babe
崮生（子虚） <2234839456@qq.com>
2025-06-19 18:02:08 +0800

    完善队列功能

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,6 +4,8 @@ import { startServer } from './server';
 import { loadConfig } from 'c12';
 import { AppConfigService, type AppConfig } from './service/AppConfigService';
 import fs from 'fs/promises';
+import { PrismaQueue } from './util/prismaQueue';
+import { prisma } from './db';
 const main = Effect.gen(function* () {
   const { config } = yield* Effect.promise(() =>
     loadConfig<AppConfig>({
@@ -29,6 +31,25 @@ const main = Effect.gen(function* () {
   }
 
   yield* seedDB.pipe(Effect.provideService(AppConfigService, config));
+
+  type MyTasks = {
+    sendEmail: {
+      payload: { to: string; subject: string };
+      result: { sent: boolean };
+    };
+  };
+  const queue = new PrismaQueue<MyTasks>({ prisma });
+  // 注册任务
+  queue.register('sendEmail', async (payload) => {
+    console.log(`Send email to ${payload.to}, subject: ${payload.subject}`);
+    throw new Error('测试错误');
+    return { sent: true };
+  });
+
+  // 启动队列
+  queue.start();
+  // 添加任务
+  // queue.add('sendEmail', { to: 'alice@example.com', subject: '欢迎 Alice' });
   yield* startServer.pipe(Effect.provideService(AppConfigService, config));
 });
 
