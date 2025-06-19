6572f6f370605a9b8e357f1bcb809e310951ea3d
崮生（子虚） <2234839456@qq.com>
2025-06-19 19:00:35 +0800

    基于队列添加定时任务功能

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -6,6 +6,7 @@ import { AppConfigService, type AppConfig } from './service/AppConfigService';
 import fs from 'fs/promises';
 import { PrismaQueue } from './util/prismaQueue';
 import { prisma } from './db';
+import { QueueScheduler } from './util/QueueScheduler';
 const main = Effect.gen(function* () {
   const { config } = yield* Effect.promise(() =>
     loadConfig<AppConfig>({
@@ -31,7 +32,14 @@ const main = Effect.gen(function* () {
   }
 
   yield* seedDB.pipe(Effect.provideService(AppConfigService, config));
+  // yield* queue_scheduler;
+  yield* startServer.pipe(Effect.provideService(AppConfigService, config));
+});
 
+Effect.runPromise(main);
+
+/** 队列与定时任务调度 */
+const queue_scheduler = Effect.gen(function* () {
   type MyTasks = {
     sendEmail: {
       payload: { to: string; subject: string };
@@ -41,16 +49,29 @@ const main = Effect.gen(function* () {
   const queue = new PrismaQueue<MyTasks>({ prisma });
   // 注册任务
   queue.register('sendEmail', async (payload) => {
-    console.log(`Send email to ${payload.to}, subject: ${payload.subject}`);
-    throw new Error('测试错误');
+    // throw new Error('failTask');
     return { sent: true };
   });
-
-  // 启动队列
-  queue.start();
+  // 启动队列v
+  // queue.start();
   // 添加任务
-  // queue.add('sendEmail', { to: 'alice@example.com', subject: '欢迎 Alice' });
-  yield* startServer.pipe(Effect.provideService(AppConfigService, config));
-});
+  setTimeout(() => {
+    // queue.add('sendEmail', { to: 'alice@example.com', subject: '欢迎 Alice' });
+  }, 1_000);
+  const scheduler = new QueueScheduler(
+    queue,
+    'tenSecondTask',
+    async () => {
+      console.log('十秒任务触发，执行业务逻辑', new Date().toISOString());
+      // 这里写你的业务逻辑，比如发出其他任务
+      return { sent: '执行成功' };
+    },
+    (lastRun) => {
+      // lastRun 是上次触发时间，返回下次触发时间 = 当前时间 + 10秒
+      return new Date(Date.now() + 10_000);
+    },
+  );
 
-Effect.runPromise(main);
+  // 启动调度，5秒后首次触发
+  // scheduler.start(new Date(Date.now() + 5000));
+});
