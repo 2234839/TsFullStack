14ad87b7ac1d34e67d986f6625b22d1788b051d8
崮生（子虚） <2234839456@qq.com>
2025-06-22 12:20:24 +0000

    重构首页文案

--- apps/website-frontend/src/components/system/CommonSettingBtns.vue
+++ apps/website-frontend/src/components/system/CommonSettingBtns.vue
@@ -1,13 +1,13 @@
 <style scoped></style>
 <template>
   <div class="flex gap-0.5 items-center">
-
     <ThemeToggle />
+    <I18nSwitch />
     <UserSettingBtn />
   </div>
 </template>
 <script setup lang="ts">
-import ThemeToggle from '@/components/system/ThemeToggle.vue'
-import UserSettingBtn from '@/components/system/UserSettingBtn.vue'
-
+  import ThemeToggle from '@/components/system/ThemeToggle.vue';
+  import UserSettingBtn from '@/components/system/UserSettingBtn.vue';
+  import I18nSwitch from '@/components/system/I18nSwitch.vue';
 </script>
--- apps/website-frontend/src/components/system/UserSettingBtn.vue
+++ apps/website-frontend/src/components/system/UserSettingBtn.vue
@@ -8,7 +8,7 @@
     icon="pi pi-power-off"
     class="p-button-rounded p-button-text p-button-plain"
     @click="authInfo_logout($route.fullPath)"
-    v-tooltip="$t('退出')" />
+    v-tooltip="$t('退出登录')" />
 </template>
 <script setup lang="ts">
   import { authInfo_isLogin, authInfo_logout } from '@/storage';
