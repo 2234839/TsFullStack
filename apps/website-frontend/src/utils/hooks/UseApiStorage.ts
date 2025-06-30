f07f9d26979f6ecac0a9bf471501c40a2b7e0ddd
崮生（子虚） <2234839456@qq.com>
2025-06-30 11:04:59 +0000

    useApiStorage 支持轮询比对远程版本号自动更新本地数据

--- apps/website-frontend/src/utils/hooks/UseApiStorage.ts
+++ apps/website-frontend/src/utils/hooks/UseApiStorage.ts
@@ -1,7 +1,15 @@
 import { API } from '@/api';
 import { authInfo, authInfo_isLogin } from '@/storage';
 import { userDataAppid } from '@/storage/userDataAppid';
-import { StorageSerializers, useStorage, useStorageAsync, useThrottleFn } from '@vueuse/core';
+import {
+  StorageSerializers,
+  useDocumentVisibility,
+  useIntervalFn,
+  useStorage,
+  useStorageAsync,
+  useThrottleFn,
+} from '@vueuse/core';
+import { ref, watchEffect } from 'vue';
 
 /** 自定义存储适配器,当用户处于登录状态时使用API存储,否则使用本地存储 */
 export function useApiStorage<T>(
@@ -14,6 +22,8 @@ export function useApiStorage<T>(
       read: (v: string) => T;
       write: (v: T) => string;
     };
+    /** 单位：毫秒，设置后启用轮询远程更新 */
+    pollingInterval?: number;
     /** 是否合并默认值,默认为false */
     mergeDefaults?: boolean;
     /** api 存储数据的 appid，默认为 userDataAppid.storage_api 的值 */
@@ -30,9 +40,18 @@ export function useApiStorage<T>(
   };
 
   const isLogin = authInfo_isLogin.value;
+  /** 目前由于同步远程数据到本地也会触发一次 setItem，导致不停的触发版本升级，暂时使用一个标志位来避免，但感觉可能还会存在bug */
+  const isSyncingFromRemote = ref(false);
+
   const setItem = useThrottleFn(
     async (key: string, value: string) => {
+      if (isSyncingFromRemote.value) {
+        isSyncingFromRemote.value = false;
+        return;
+      }
+
       const parsed = JSON.parse(value);
+      localVersion.value += 1;
       await API.db.userData.upsert({
         where: {
           userId_key_appId: {
@@ -40,28 +59,37 @@ export function useApiStorage<T>(
             appId,
             userId: authInfo.value.userId,
           },
+          version: {
+            // 确保版本号不超过本地版本号
+            lte: localVersion.value,
+          },
         },
         create: {
           key,
           appId,
           userId: authInfo.value.userId,
           data: parsed,
+          version: localVersion.value,
         },
         update: {
           data: parsed,
-          version: { increment: 1 },
+          version: localVersion.value,
         },
       });
     },
     opts?.throttle ?? 3500,
     true,
   );
+
+  const localVersion = ref<number>(0);
+
   const apiStorageAsync = {
     async getItem(key: string) {
       const res = await API.db.userData.findFirst({
         where: { key, appId, userId: authInfo.value.userId },
       });
       if (!res) return null;
+      localVersion.value = res.version;
       const data = await res.data;
       return JSON.stringify(data);
     },
@@ -81,15 +109,60 @@ export function useApiStorage<T>(
     },
   };
 
-  if (storage === 'auto') {
-    return isLogin
-      ? useStorageAsync<T>(key, defaultValue, apiStorageAsync, options)
-      : useStorage<T>(key, defaultValue, undefined, options);
+  function createStorage() {
+    if (storage === 'auto') {
+      return isLogin
+        ? useStorageAsync<T>(key, defaultValue, apiStorageAsync, options)
+        : useStorage<T>(key, defaultValue, undefined, options);
+    }
+
+    if (storage === localStorage || storage === sessionStorage) {
+      return useStorage<T>(key, defaultValue, storage, options);
+    }
+
+    return useStorageAsync<T>(key, defaultValue, apiStorageAsync, options);
+  }
+  const state = createStorage();
+
+  if (opts?.pollingInterval && isLogin) {
+    const { pause, resume, isActive } = useIntervalFn(async () => {
+      try {
+        const res = await API.db.userData.findFirst({
+          where: { key, appId, userId: authInfo.value.userId },
+          select: { version: true },
+        });
+
+        if (res && res.version > localVersion.value) {
+          const fullRes = await API.db.userData.findFirst({
+            where: { key, appId, userId: authInfo.value.userId },
+            select: { version: true, data: true },
+          });
+          if (!fullRes) {
+            throw new Error('Failed to fetch full data');
+          }
+
+          localVersion.value = fullRes.version;
+          // 更新本地状态（这段取决于 storage 类型）
+          const parsed = serializer.read(JSON.stringify(fullRes.data));
+          isSyncingFromRemote.value = true;
+          state.value = parsed;
+        }
+      } catch (err) {
+        console.warn(`[useApiStorage] polling error:`, err);
+      }
+    }, opts.pollingInterval);
+
+    const visibility = useDocumentVisibility();
+
+    /** 当页面不活跃时停止轮询 */
+    watchEffect(() => {
+      if (visibility.value === 'visible' && !isActive.value) {
+        resume();
+      } else if (visibility.value === 'hidden') {
+        pause();
+      }
+    });
   }
 
-  if (storage === localStorage || storage === sessionStorage) {
-    return useStorage<T>(key, defaultValue, storage, options);
-  }
-
-  return useStorageAsync<T>(key, defaultValue, apiStorageAsync, options);
+  return state;
 }
