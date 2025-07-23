7f38bde48d27e9ab39b4227b31487b8ebd2a893f
崮生（子虚） <2234839456@qq.com>
2025-07-23 12:28:03 +0000

    重构api位置

--- apps/backend/src/api/app/file.ts
+++ apps/backend/src/api/app/file.ts
@@ -1,3 +1,66 @@
-export const appFileApi = {
+import { Effect } from 'effect';
+import { AuthService } from '../../service/Auth';
+import { File as FileModel, StorageType } from '@zenstackhq/runtime/models';
+import { readFile, writeFile } from 'fs/promises';
+import { join } from 'path/posix';
+import { v7 as uuidv7 } from 'uuid';
+import { MsgError } from '../../util/error';
+import { AppConfigService } from '../../service/AppConfigService';
 
-}
+export const fileApi = {
+  /** file 这种二进制对象传递比较特殊，使用 superJSON 的话会大幅增加请求大小，所以在数据传输层做了特殊处理，这里也只能接受一个参数 */
+  upload(file: File) {
+    return Effect.gen(function* () {
+      const auth = yield* AuthService;
+      const fileId = uuidv7();
+      const appConfig = yield* AppConfigService;
+      const filePath = join(appConfig.uploadDir, fileId);
+      const arrayBuffer = yield* Effect.promise(() => file.arrayBuffer());
+      const buffer = Buffer.from(arrayBuffer);
+      yield* Effect.promise(() => writeFile(filePath, buffer));
+
+      const res = yield* Effect.promise(() =>
+        auth.db.file.create({
+          data: {
+            filename: file.name,
+            mimetype: file.type,
+            path: filePath,
+            size: file.size, // 文件大小
+            storageType: StorageType.LOCAL,
+            authorId: auth.user.id,
+          },
+        }),
+      );
+
+      return res;
+    });
+  },
+  file(id: FileModel['id']) {
+    return Effect.gen(function* () {
+      const auth = yield* AuthService;
+
+      const fileRow = yield* Effect.promise(() =>
+        auth.db.file.findUnique({
+          where: {
+            id,
+          },
+        }),
+      );
+      if (!fileRow) {
+        throw MsgError.msg('File not found');
+      }
+      // 读取文件内容
+      const filePath = fileRow?.path;
+      const fileBuffer = yield* Effect.promise(() =>
+        readFile(filePath).catch((e) => {
+          throw MsgError.msg('读取文件失败' + e);
+        }),
+      );
+      const file = new File([fileBuffer], fileRow.filename, {
+        type: fileRow.mimetype,
+        lastModified: fileRow.updated.getTime(),
+      });
+      return file;
+    });
+  },
+};
