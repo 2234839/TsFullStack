0fb782390e800710945d4fd11eeadafc6b488887
崮生（子虚） <2234839456@qq.com>
2025-08-03 10:48:10 +0800

    clean

---
+++ apps/InfoFlow/entrypoints/content.ts
@@ -0,0 +1,23 @@
+import { createApp, createShadowRootUi, defineContentScript } from '#imports';
+import App from './content/app.vue';
+export default defineContentScript({
+  matches: ['<all_urls>'],
+  cssInjectionMode: 'ui',
+  async main(ctx) {
+    const ui = await createShadowRootUi(ctx, {
+      name: 'example-ui',
+      position: 'inline',
+      anchor: 'body',
+      onMount: (container) => {
+        const app = createApp(App);
+        app.mount(container);
+        return app;
+      },
+      onRemove: (app) => {
+        app?.unmount();
+      },
+    });
+    console.log('[ui]', ui);
+    ui.mount();
+  },
+});
