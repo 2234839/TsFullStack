c2dc7fb0d35795017d2387e842f53a5113fca9bc
崮生（子虚） <2234839456@qq.com>
2025-08-03 10:46:39 +0800

    clean

--- apps/InfoFlow/storage/auth.ts
+++
@@ -1,9 +0,0 @@
-import { useStorage } from '@/utils/userStorage.hook';
-
-export const { data: authStore, loadingPromise: authStoreLoadingPromise } = useStorage(
-  'local:auth_v0',
-  () => ({
-    token: null as null | string,
-    userId: null as null | string,
-  }),
-);
