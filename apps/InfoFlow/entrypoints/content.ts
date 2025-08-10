073bdcde91a16ca301610128e34409f84ee5aa99
崮生（子虚） <2234839456@qq.com>
2025-08-10 19:53:42 +0800

    升级依赖

--- apps/InfoFlow/entrypoints/content.ts
+++ apps/InfoFlow/entrypoints/content.ts
@@ -1,15 +1,32 @@
 import { createApp, createShadowRootUi, defineContentScript } from '#imports';
+import '~/assets/tailwindcss.css';
+import 'primeicons/primeicons.css';
+import 'primevue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './content/app.vue';
+import { ConfirmationService, ToastService } from 'primevue';
+
 export default defineContentScript({
   matches: ['<all_urls>'],
   cssInjectionMode: 'ui',
   async main(ctx) {
+    // createShadowRootUi 模式会导致无法正确的加载primevue组件的样式
     const ui = await createShadowRootUi(ctx, {
-      name: 'example-ui',
+      name: 'infoflow-ui',
+    // const ui = await createIntegratedUi(ctx, {
       position: 'inline',
       anchor: 'body',
       onMount: (container) => {
         const app = createApp(App);
+        app.use(PrimeVue, {
+          theme: {
+            preset: Aura,
+
+          },
+        });
+        app.use(ToastService);
+        app.use(ConfirmationService);
         app.mount(container);
         return app;
       },
@@ -17,7 +34,6 @@ export default defineContentScript({
         app?.unmount();
       },
     });
-    console.log('[ui]', ui);
     ui.mount();
   },
 });
--- apps/InfoFlow/entrypoints/popup/main.ts
+++ apps/InfoFlow/entrypoints/popup/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
-import './style.css';
+import '~/assets/tailwindcss.css';
 import 'primeicons/primeicons.css'
 import { ConfirmationService, ToastService } from 'primevue';
 
