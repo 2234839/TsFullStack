d756114f31aca00f9600f8c36d906797a1204d30
崮生（子虚） <2234839456@qq.com>
2025-08-24 11:12:24 +0000

    实现 useWxtStorage 的响应式数据变化

--- apps/InfoFlow/entrypoints/content/app.vue
+++ apps/InfoFlow/entrypoints/content/app.vue
@@ -1,13 +1,12 @@
-<style scoped>
-
-</style>
+<style scoped></style>
 <template>
   <div class="fixed bottom-0">
     <div class="bg-red-300">2222</div>
-    <Button>1111</Button>
-
+    v: {{ infoFlowConfig }}
+    <Button @click="infoFlowConfig!.test++">1111</Button>
   </div>
 </template>
 <script setup lang="ts">
+  import { infoFlowConfig } from '@/storage/config';
   import Button from './Button.vue';
 </script>
