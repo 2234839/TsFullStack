b096f39e75eb8b160d72a62bfa6478168ef2dd4b
崮生（子虚） <2234839456@qq.com>
2025-08-24 11:17:04 +0000

    优化 useWxtStorage

--- apps/InfoFlow/storage/config.ts
+++ apps/InfoFlow/storage/config.ts
@@ -1,50 +1,8 @@
-import { storage } from '#imports';
+import { useWxtStorage } from './storageUtil';
 
-export const infoFlowConfig = useWxtStorage(
-  storage.defineItem('local:infoFlowConfig', {
-    fallback: {
-      test: 33,
-    },
-    version: 1,
-  }),
-);
-
-export function useWxtStorage<T>(wxtStorage: globalThis.WxtStorageItem<T, {}>) {
-  const storageRef = ref<T | undefined>();
-  let isUpdatingFromStorage = false;
-
-  const unWatch = wxtStorage.watch((v) => {
-    isUpdatingFromStorage = true;
-    storageRef.value = v;
-    console.log('watch', v);
-    setTimeout(() => {
-      isUpdatingFromStorage = false;
-    }, 0);
-  });
-  onUnmounted(unWatch);
-
-  wxtStorage.getValue().then((v) => {
-    isUpdatingFromStorage = true;
-    storageRef.value = v;
-    setTimeout(() => {
-      isUpdatingFromStorage = false;
-    }, 0);
-  });
-
-  watch(
-    storageRef,
-    (v) => {
-      if (isUpdatingFromStorage) return;
-
-      if (v !== undefined) {
-        wxtStorage.setValue(v);
-        console.log('save', v);
-      } else {
-        wxtStorage.removeValue();
-      }
-    },
-    { deep: true },
-  );
-
-  return storageRef;
-}
+export const infoFlowConfig = useWxtStorage('local:infoFlowConfig', {
+  fallback: {
+    test: 33,
+  },
+  version: 1,
+});
---
+++ apps/InfoFlow/storage/storageUtil.ts
@@ -0,0 +1,52 @@
+import { storage } from '#imports';
+import { watchDebounced } from '@vueuse/core';
+
+type WxtStorageItemOptions<Tvalue> = Parameters<typeof storage.defineItem<Tvalue>>[1];
+type WxtStorageKey = Parameters<typeof storage.defineItem>[0];
+
+/** 设置一个响应式的插件存储数据 */
+export function useWxtStorage<TValue>(
+  key: WxtStorageKey,
+  options: WxtStorageItemOptions<TValue> &
+    /** 约束 fallback 必传，这样可以避免 storageRef 为 undefined 的情况出现（除非fallback本身就支持 undefined） */
+    Required<Pick<WxtStorageItemOptions<TValue>, 'fallback'>>,
+) {
+  const wxtStorage = storage.defineItem(key, options);
+  const storageRef = ref<TValue>(options.fallback);
+  let isUpdatingFromStorage = false;
+
+  const unWatch = wxtStorage.watch((v) => {
+    isUpdatingFromStorage = true;
+    storageRef.value = v;
+    console.log('watch', v);
+    setTimeout(() => {
+      isUpdatingFromStorage = false;
+    }, 0);
+  });
+  onUnmounted(unWatch);
+
+  wxtStorage.getValue().then((v) => {
+    isUpdatingFromStorage = true;
+    storageRef.value = v;
+    setTimeout(() => {
+      isUpdatingFromStorage = false;
+    }, 0);
+  });
+
+  watchDebounced(
+    storageRef,
+    (v) => {
+      if (isUpdatingFromStorage) return;
+
+      if (v !== undefined) {
+        wxtStorage.setValue(storageRef.value);
+        console.log('save', storageRef.value);
+      } else {
+        wxtStorage.removeValue();
+      }
+    },
+    { deep: true, debounce: 30, maxWait: 100 },
+  );
+
+  return storageRef;
+}
