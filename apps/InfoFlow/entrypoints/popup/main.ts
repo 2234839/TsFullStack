b68cafc7e80cbd3b5e9e70ed65e57b1e2db1a4a5
崮生（子虚） <2234839456@qq.com>
2025-08-26 11:33:07 +0000

    clean

--- apps/InfoFlow/entrypoints/popup/main.ts
+++ apps/InfoFlow/entrypoints/popup/main.ts
@@ -1,18 +1,4 @@
-import { createApp } from 'vue';
+import { setupApp } from '@/utils/setupApp';
 import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
-import '~/src/styles/app.css';
-import 'primeicons/primeicons.css'
-import { ConfirmationService, ToastService } from 'primevue';
 
-const app = createApp(App);
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-});
-app.use(ToastService);
-app.use(ConfirmationService);
-
-app.mount('#app');
+setupApp(App);
