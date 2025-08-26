1e94a72bd6de0b13d59c39708a0308f0b3875f70
崮生（子虚） <2234839456@qq.com>
2025-08-26 11:21:10 +0000

    fix 样式文件路径错误

--- apps/InfoFlow/entrypoints/popup/main.ts
+++ apps/InfoFlow/entrypoints/popup/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
-import '~/assets/tailwindcss.css';
+import '~/src/styles/app.css';
 import 'primeicons/primeicons.css'
 import { ConfirmationService, ToastService } from 'primevue';
 
