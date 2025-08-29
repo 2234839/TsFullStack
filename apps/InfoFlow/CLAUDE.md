2edba9dd8b63f55a63a5063996c4e1d5bcd47ebc
崮生（子虚） <2234839456@qq.com>
2025-08-29 19:24:59 +0800

    更新依赖

---
+++ apps/InfoFlow/CLAUDE.md
@@ -0,0 +1,5 @@
+## InfoFlow 浏览器扩展
+
+## 技术栈
+
+- vite ： 配置文件在 wxt.config.ts 而非  vite.config.ts
--- apps/InfoFlow/wxt.config.ts
+++ apps/InfoFlow/wxt.config.ts
@@ -1,19 +1,24 @@
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
   // @ts-ignore
   vite: () => ({
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      VueDevTools({
+        // https://devtools.vuejs.org/help/faq#how-to-work-with-wxt
+        // 对于 options 页面无效 https://github.com/vuejs/devtools/issues/727
+        // appendTo: '/entrypoints/options/main.ts',
+        // appendTo: '/entrypoints/popup/main.ts',
+      }),
+    ],
   }),
-  // 修改了这里记得去浏览器重载扩展
-  // imports: true,
   manifest: {
     permissions: ['storage', 'tabs', 'notifications', 'activeTab'],
-
   },
-
-
-  imports:false
+  /** 禁止自动 import，更清晰的依赖流 */
+  imports: false,
 });
